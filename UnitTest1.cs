af0b64d4e20e26579de60679f669f21d49da1e76
Clerida Gounden <clerida.gounden@gmail.com>
2023-01-29 19:08:26 +0200

    Cleaned up file

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,69 +10,42 @@ namespace SeleniumWikipediaDemo
         [SetUp]
         public void Setup()
         {
-      
+
         }
 
         [Test]
         public void Test()
         {
+            //Navigate to Chrome Browser
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.wikipedia.org/");
 
+            //Input search Text - Automation Testing
             IWebElement searchBox = driver.FindElement(By.Id("searchInput"));
             searchBox.SendKeys("Automation Testing");
 
+            //Click on search button and takes you to Test automation page
             IWebElement searchButton = driver.FindElement(By.XPath("//button[@type='submit']"));
             searchButton.Click();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //Open Browser
-            //*IWebDriver webDriver = new ChromeDriver();
-            /*Navigate to site
-            webDriver.Navigate().GoToUrl("https://en.wikipedia.org/wiki/Main_Page");
-
-           IWebDriver  search = driver.FindElement(By.Id("searchInput"));
-           searchBox.SendKeys("Automation testing");*/
-
-           // IWebElement searchButton = driver.FindElement(By.ClassName("Search"));
-           // searchButton.Click();
-
-          //  ElementClickInterceptedException 
-
-           // IWebDriver driver.FindElement(By.Id("searchButton")).Click();
-
-            //IWebElement searchButton = driver.FindElement(By.XPath("//button[@id='searchform']"));
-            //searchButton.Click();
-
-            //Using click method to submit search
-            // searchButton.SendKeys
-
-
-
-
-
-            //Wait for the page to load
-            System.Threading.Thread.Sleep(5000);
-
-            // Get the title of the page and print it
-            Console.WriteLine(driver.Title);
-
-            // Close the browser
-            driver.Quit();
+            //This code verifies search results page contains the correct page title "Test automation - Wikipedia"
+            //This code verifies search result contains the text "Test automation can automate some repetitive
+            //but necessary tasks in a formalized testing process"
 
         }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+            
+  
